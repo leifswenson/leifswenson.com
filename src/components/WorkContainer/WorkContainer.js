80df69fa78735233b5053f0b812ab25919f2f302
Leif Swenson <leifswenson@gmail.com>
2019-10-03 13:03:40 -0500

    convert site to Gatsby

--- src/components/WorkContainer/WorkContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { CardColumns } from "reactstrap";
-import "./WorkContainer.css";
-
-const WorkContainer = props => {
-  return <CardColumns>{props.children}</CardColumns>;
-};
-
-export default WorkContainer;
