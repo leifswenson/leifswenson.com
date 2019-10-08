271c25c0bd91668a427d49ea7b2b4d689e1d1607
Leif Swenson <leifswenson@gmail.com>
2019-10-08 15:46:11 -0500

    project style

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import projectStyles from "./project.module.scss";
+
 const Project = props => {
   return (
     <section
@@ -9,21 +11,35 @@ const Project = props => {
         color: `${props.workData.textColor}`
       }}
     >
-      <div className='container'>
+      <div className={`'container' ${projectStyles.container}`}>
         <div className='row'>
           <div className='col md 12'>
-            <div className='project-image'>
+            {/* <div className='project-image'>
               <img src={props.workData.image} alt={props.workData.title} />
-            </div>
-            <div className='project-info'>
-              <h2 className='project-heading'>{props.workData.title}</h2>
-              <h3 className='project-type'>{props.workData.category}</h3>
-              <p className='project-description'>
+            </div> */}
+            <div className={projectStyles.info}>
+              <h2 className={projectStyles.heading}>{props.workData.title}</h2>
+              <h3 className={projectStyles.type}>{props.workData.category}</h3>
+              <p className={projectStyles.description}>
                 {props.workData.description}
               </p>
-              <a href={props.workData.link} target='blank'>
-                <button>Check it out</button>
-              </a>
+              <div className={projectStyles.buttonArea}>
+                <a
+                  className={projectStyles.link}
+                  href={props.workData.link}
+                  target='blank'
+                >
+                  <button
+                    style={{
+                      backgroundColor: `${props.workData.buttonColor}`,
+                      color: `${props.workData.textColor}`
+                    }}
+                    className={`btn btn-primary ${projectStyles.button}`}
+                  >
+                    Check it out
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
