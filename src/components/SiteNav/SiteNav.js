7925ad59b0d519502766dd8c12b15865af7cd546
Leif Swenson <leifswenson@gmail.com>
2019-10-29 10:36:18 -0500

    fixed bug in navbar with work link

--- src/components/SiteNav/SiteNav.js
+++ src/components/SiteNav/SiteNav.js
@@ -14,7 +14,7 @@ const SiteNav = ({ siteTitle }) => {
       <Navbar
         className={`fixed-top ${navStyles.container}`}
         bg="light"
-        expand="sm"
+        expand="md"
       >
         <Navbar.Brand as="span">
           <Link
@@ -28,55 +28,63 @@ const SiteNav = ({ siteTitle }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link eventKey="/">
-              <Link
-                className={navStyles.navItem}
-                activeClassName={navStyles.activeNavItem}
-                to="/"
-              >
-                Home
-              </Link>
-            </Nav.Link>
-
-            <Nav.Link eventKey="work">
-              <Link
-                className={navStyles.navItem}
-                activeClassName={navStyles.activeNavItem}
-                to="/work"
-              >
-                Work
-              </Link>
-            </Nav.Link>
-
-            <Nav.Link eventKey="contact">
-              <Link
-                className={navStyles.navItem}
-                activeClassName={navStyles.activeNavItem}
-                to="/contact"
-              >
-                Contact
-              </Link>
-            </Nav.Link>
+            <Nav.Item>
+              <Nav.Link eventKey="/">
+                <Link
+                  className={navStyles.navItem}
+                  activeClassName={navStyles.activeNavItem}
+                  to="/"
+                >
+                  Home
+                </Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="work">
+                <Link
+                  className={navStyles.navItem}
+                  activeClassName={navStyles.activeNavItem}
+                  to="/work"
+                >
+                  Work
+                </Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="contact">
+                <Link
+                  className={navStyles.navItem}
+                  activeClassName={navStyles.activeNavItem}
+                  to="/contact"
+                >
+                  Contact
+                </Link>
+              </Nav.Link>
+            </Nav.Item>
 
             <Nav>
-              <Nav.Link href="linkedin">
-                <a
-                  className={navStyles.navItem}
-                  href="https://github.com/leifswenson"
-                  target="blank"
-                >
-                  <FontAwesomeIcon icon={faGithubSquare} size="2x" />
-                </a>
-              </Nav.Link>
-              <Nav.Link href="github">
-                <a
-                  className={navStyles.navItem}
-                  href="https://www.linkedin.com/in/leif-swenson/"
-                  target="blank"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                </a>
-              </Nav.Link>
+              <Nav.Item>
+                <Nav.Link href="linkedin">
+                  <a
+                    className={navStyles.navItem}
+                    href="https://github.com/leifswenson"
+                    target="blank"
+                  >
+                    <FontAwesomeIcon icon={faGithubSquare} size="2x" />
+                  </a>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="github">
+                  <a
+                    className={navStyles.navItem}
+                    href="https://www.linkedin.com/in/leif-swenson/"
+                    target="blank"
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                  </a>
+                </Nav.Link>
+              </Nav.Item>
             </Nav>
           </Nav>
         </Navbar.Collapse>
